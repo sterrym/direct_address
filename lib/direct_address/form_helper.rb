15fae9adf2dd575e5855f6e6039b0c1d3da949a9
Mike Nelson <mn@mcaddie.com>
2010-02-17 15:41:51 -0500

    js is not ruby

--- lib/direct_address/form_helper.rb
+++ lib/direct_address/form_helper.rb
@@ -18,7 +18,7 @@ module ActionView
 					region_options = options_for_select([['Select a Region', nil]] + @regions.collect{|region| [region.name, region.id]}, region_id)
 					html = select_tag("#{object_string}[country_id]", country_options, :id => "#{object_string}_country_id")
 					html << select_tag("#{object_string}[region_id]", region_options, :id => "#{object_string}_region_id")
-					html << "<script type=\"text/javascript\">if(Prototype){Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{object_string}_country_id', region_select_id : '#{object_string}_region_id'});});}else{document.getElementById('#{object_string}_country_id').style.display = 'none';document.getElementById('#{object_string}_region_id').style.display = 'none';}</script>"
+					html << "<script type=\"text/javascript\">if(typeof(Prototype) == 'undefined'){Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{object_string}_country_id', region_select_id : '#{object_string}_region_id'});});}else{document.getElementById('#{object_string}_country_id').style.display = 'none';document.getElementById('#{object_string}_region_id').style.display = 'none';}</script>"
 					html
 				end
 			end
