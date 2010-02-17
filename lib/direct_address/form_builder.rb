f1fa1f5185954b1e965802cf15b8b4377228a30a
Mike Nelson <mn@mcaddie.com>
2010-02-17 15:39:33 -0500

    Safeguard against lack of prototype

--- lib/direct_address/form_builder.rb
+++ lib/direct_address/form_builder.rb
@@ -14,7 +14,7 @@ module ActionView
 					select_name = self.object_name
 					html = self.select(:country_id, ([['Select a Country', nil]] + @countries.collect{|country| [country.name, country.id]}))
 					html << self.select(:region_id, ([['Select a Region', nil]] + (self.object && self.object.country && self.object.country.regions.collect{|region| [region.name, region.id]} || [])))
-					html << "<script type=\"text/javascript\">Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{select_name}_country_id', region_select_id : '#{select_name}_region_id'})});</script>"
+					html << "<script type=\"text/javascript\">if(Prototype){Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{select_name}_country_id', region_select_id : '#{select_name}_region_id'})});}else{document.getElementById('#{select_name}_country_id').style.display = 'none';document.getElementById('#{select_name}_region_id').style.display = 'none';}</script>"
 					html
 				end
 			end
