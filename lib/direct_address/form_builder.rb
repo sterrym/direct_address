13570ab59d62cd5dcefbab917a64865948409709
Mike Nelson <mn@mcaddie.com>
2010-02-17 16:31:46 -0500

    DIY

--- lib/direct_address/form_builder.rb
+++ lib/direct_address/form_builder.rb
@@ -11,9 +11,10 @@ module ActionView
 			module InstanceMethods
 				def country_select
 					@countries ||= Country.all
+					sanitized_object_id = self.object_name.split('[').collect{|s| (s = s.gsub(']','')) && s.size > 0 && s || nil}.compact.join('_')
 					html = self.select(:country_id, ([['Select a Country', nil]] + @countries.collect{|country| [country.name, country.id]}))
 					html << self.select(:region_id, ([['Select a Region', nil]] + (self.object && self.object.country && self.object.country.regions.collect{|region| [region.name, region.id]} || [])))
-					html << "<script type=\"text/javascript\">if(typeof(Prototype) != 'undefined'){Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{sanitized_object_name}_country_id', region_select_id : '#{sanitized_object_name}_region_id'})});}else{document.getElementById('#{sanitized_object_name}_country_id').style.display = 'none';document.getElementById('#{sanitized_object_name}_region_id').style.display = 'none';}</script>"
+					html << "<script type=\"text/javascript\">if(typeof(Prototype) != 'undefined'){Element.observe(window, 'load', function(){new CountrySelect({country_select_id : '#{sanitized_object_id}_country_id', region_select_id : '#{sanitized_object_id}_region_id'})});}else{document.getElementById('#{sanitized_object_id}_country_id').style.display = 'none';document.getElementById('#{sanitized_object_id}_region_id').style.display = 'none';}</script>"
 					html
 				end
 			end
