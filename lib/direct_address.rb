0a9a0cb4d8014b190a33d1485ea83b440ef93861
Mike Nelson <mn@mcaddie.com>
2010-02-22 17:41:21 -0500

    using file.join instead

--- lib/direct_address.rb
+++ lib/direct_address.rb
@@ -1,5 +1,5 @@
 [:form_builder, :form_helper, :acts_as_addressable].each do |file|
-	require File.dirname(__FILE__) + '/direct_address/' + file.to_s
+	require File.join(File.dirname(__FILE__), 'direct_address', file.to_s)
 end
 
-require File.dirname(__FILE__) + '/direct_address/harvester/geoname.rb'
+require File.join(File.dirname(__FILE__), 'direct_address', 'harvesters', 'geoname.rb')
