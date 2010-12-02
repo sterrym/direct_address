3212e8369ddbcd1b0b40dc610af23651d090d766
Tim Glen <tim@tag.ca>
2010-12-02 15:35:44 -0500

    Added html_safe support

--- lib/direct_address/form_builder.rb
+++ lib/direct_address/form_builder.rb
@@ -20,14 +20,16 @@ module ActionView
           html = ''
           html << self.label(:country) if options[:include_labels]
           html << self.select(:country_id, ([['Select a Country', nil]] + @countries.collect{|country| [country.name, country.id]}))
-          options[:nested_in] ? self.template.content_tag(options[:nested_in], options[:nested_in_options]) { html } : html
+          html = options[:nested_in] ? self.template.content_tag(options[:nested_in], options[:nested_in_options]) { html } : html
+          html.respond_to?(:html_safe) ? html.html_safe : html
         end
 
         def country_select_region_field(options = {})
           html = ''
           html << self.label(:region) if options[:include_labels]
           html << self.select(:region_id, ([['Select a Region', nil]] + (self.object && self.object.country && self.object.country.regions.collect{|region| [region.name, region.id]} || [])))
-          options[:nested_in] ? self.template.content_tag(options[:nested_in], options[:nested_in_options]) { html } : html
+          html = options[:nested_in] ? self.template.content_tag(options[:nested_in], options[:nested_in_options]) { html } : html
+          html.respond_to?(:html_safe) ? html.html_safe : html
         end
         
         def country_select_javascript(options = {})
@@ -37,7 +39,8 @@ module ActionView
           else
             js = "<script type=\"text/javascript\">if(typeof(Prototype) != 'undefined'){Element.observe(window, 'load', function(){if(typeof(CountrySelect) == 'undefined'){try{console.log('country_select.js not loaded.');} catch (e) {};return;};new CountrySelect({country_select_id : '#{sanitized_object_id}_country_id', region_select_id : '#{sanitized_object_id}_region_id'})});}else{document.getElementById('#{sanitized_object_id}_country_id').style.display = 'none';document.getElementById('#{sanitized_object_id}_region_id').style.display = 'none';}</script>"
           end
-          options[:nested_in] ? self.template.content_tag(options[:nested_in], { :style => "display:none" }) { js } : js
+          html = options[:nested_in] ? self.template.content_tag(options[:nested_in], { :style => "display:none" }) { js } : js
+          html.respond_to?(:html_safe) ? html.html_safe : html
         end
       end
     end
